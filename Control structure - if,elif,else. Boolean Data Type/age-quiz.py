7ab4ed6a98f3541cdd01b2a0020d036fb35abaa2
Dmytro Vorobiov <benjaminemoore28@gmail.com>
2024-01-29 12:52:12 +0000

    There is review feedback implamentation

--- Control structure - if,elif,else. Boolean Data Type/age-quiz.py
+++ Control structure - if,elif,else. Boolean Data Type/age-quiz.py
@@ -1,13 +1,16 @@
+# Let's get the user's age
 age = int(input("Enter your current age, please: "))
-if age < 13:
-    print("You qualify for the kiddie discount.")
+
+# Classify the user based on their age
+if age > 100:
+    print("Sorry you are dead!")
+elif age >= 65:
+    print("Enjoy your retirement")
+elif age >= 40:
+    print("You're over the hill")
 elif age == 21:
     print("Congrats on your 21st!")
-elif age >= 40:
-    print("You're over the hill.")
-elif age >= 65:
-    print("Enjoy your retirement!")
-elif age > 100:
-    print("Sorry you are dead!")
+elif age < 13:
+    print("You qualify for the kiddie discount")
 else:
-    print("Age is but a number.")
+    print("Age is but a number")
